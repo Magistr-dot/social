c19f740373fa2f9c55fb4ac8c7abccb6d28be522
Alex <nesterov178@gmail.com>
2020-12-25 16:22:20 +0300

    update

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div>
+            <a>1222222222222</a>
+            <a>2</a>
+            <a>3</a>
+        </div>
+    );
+}
+export default Header
---
+++ src/Tech.js
@@ -0,0 +1,12 @@
+const Tech = () => {
+    return(
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+            </ul>
+        </div>
+    )
+}
+export default Tech
