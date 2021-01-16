b143de24ada4256a3efdeeb343d0033bfd88d8c0
Alex <nesterov178@gmail.com>
2021-01-16 20:15:33 +0300

    test commit blyat

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from "./redux/state";
 
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App appstate = {state}/>
